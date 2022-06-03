092fe34bfb8ecd5c09c8b86a6c6de9929df0c59e
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-03 18:22:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex3_Laborator5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 3
+            //Scrieti un program care va numara toate aparitiile unui caracter intr - un sir
+            //de caractere.Atat caracterul cautat cat si sirul de caracter vor fi citite de la
+            //tastatura
+
+            Console.WriteLine("Introduceti un sir de caractere");
+            string sir = Console.ReadLine();
+            Console.WriteLine("Introduceti un caracter");
+            char caracter = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int aparitiiCaracter = NumerotareCaracter(sir, caracter);
+            Console.WriteLine($"Caracterul '{caracter}' a aparut de {aparitiiCaracter} ori in sirul introdus.", caracter, aparitiiCaracter);
+        }
+        static int NumerotareCaracter(string sir,char caracter)
+        {
+            int numerotare = 0;
+            foreach (char simbol in sir)
+            {
+                if (simbol==caracter)
+                {
+                    numerotare++;
+                }
+            }
+            return numerotare; 
+        }
+    }
+}
